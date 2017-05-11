a4ba019f8f103c01981c4bc45b55906e2846ae4a
XanxusX <tako30215@hotmail.com>
2017-05-11 15:13:58 +0800

    first commit

--- crawler-for-hackmd.py
+++ crawler-for-hackmd.py
@@ -2,11 +2,20 @@ import requests
 import json
 from bs4 import BeautifulSoup
 import lxml
+import codecs
 
 res = requests.get('https://hackmd.io/s/HkbDAi51Z')
 soup = BeautifulSoup(res.text, "lxml")
 
-content = soup.select('#doc')[0].text
+filename = soup.select('#doc')[0].text.split('###').replace(' ')[0]
+print(filename)
+content = soup.select('#doc')[0].text.split('##')
 
-print(content)
+a = json.dumps(content, ensure_ascii=False)
+#print (a)
 
+file = open("content.json", "w", encoding='utf-8')
+
+output = {"content": content}
+json.dump(output,file, ensure_ascii=False)
+file.close()
