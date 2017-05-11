c9c6353bea04ae505c6f3c95db758c51c4877138
XanxusX <tako30215@hotmail.com>
2017-05-11 11:45:12 +0800

    Revert "first commit"
    This reverts commit 97f5cc18edf9f212905bebf0caa8fcdce3b9ee1c.

--- crawler-for-hackmd.py
+++ crawler-for-hackmd.py
@@ -10,26 +10,3 @@ content = soup.select('#doc')[0].text
 
 print(content)
 
-"""url= "http://www.alternate.nl/html/product/listing.html?navId=11622&tk=7&lk=9419"
-r = requests.get(url) 
-soup = BeautifulSoup(r.content)
-
-product = Product()
-
-g_data = soup.find_all("div", {"class": "listRow"}) 
-for item in g_data: 
-    try: 
-        product.set_<field_name>(item.find_all("span", {"class": "name"})[0].text)
-        product.set_<field_name>("span", {"class": "additional"})[0].text
-        product.set_<field_name>("span", {"class": "info"})[0].text
-        product.set_<field_name>("span", {"class": "info"})[1].text
-        product.set_<field_name>("span", {"class": "info"})[2].text
-        product.set_<field_name>("span", {"class": "price right right10"})[0].text
-    except: 
-        pass  
-
-import json
-file = open("filename", "w")
-output = {"product1": product}
-json.dump(output, file)
-file.close()"""
