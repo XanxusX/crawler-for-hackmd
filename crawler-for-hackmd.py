f8d3ca913fe32fa7d39a7c7151a66052fb3ff2dc
XanxusX <tako30215@hotmail.com>
2017-05-12 14:38:56 +0800

    sample output

--- crawler-for-hackmd.py
+++ crawler-for-hackmd.py
@@ -1,3 +1,4 @@
+
 import requests
 import json
 from bs4 import BeautifulSoup
@@ -7,15 +8,23 @@ import codecs
 res = requests.get('https://hackmd.io/s/HkbDAi51Z')
 soup = BeautifulSoup(res.text, "lxml")
 
-filename = soup.select('#doc')[0].text.split('###').replace(' ')[0]
-print(filename)
+filename = ''.join(soup.select('#doc')[0].text.split('###')[0]).replace('\n','').replace('#','')+'.json'
 content = soup.select('#doc')[0].text.split('##')
+
+for i in range(3):
+    del content[0]
+
+for c in content:
+    c1 = c.split['\n\n 1']
+
+content = [{"title": x} for x in content ]
+
 
 a = json.dumps(content, ensure_ascii=False)
 #print (a)
 
-file = open("content.json", "w", encoding='utf-8')
+file = open(filename, "w", encoding='utf-8')
 
-output = {"content": content}
+output = {0: content}
 json.dump(output,file, ensure_ascii=False)
 file.close()
