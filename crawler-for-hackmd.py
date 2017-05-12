ffe73cfd50dd4743010b189954068c31fe30a0b0
XanxusX <tako30215@hotmail.com>
2017-05-12 17:18:37 +0800

    read txt

--- crawler-for-hackmd.py
+++ crawler-for-hackmd.py
@@ -3,28 +3,32 @@ import requests
 import json
 from bs4 import BeautifulSoup
 import lxml
-import codecs
 
-res = requests.get('https://hackmd.io/s/HkbDAi51Z')
+p=[]
+'''res = requests.get('https://hackmd.io/s/HkbDAi51Z')
 soup = BeautifulSoup(res.text, "lxml")
 
 filename = ''.join(soup.select('#doc')[0].text.split('###')[0]).replace('\n','').replace('#','')+'.json'
-content = soup.select('#doc')[0].text.split('##')
+content = soup.select('#doc')[0].text.split('##')'''
+file = open('0.txt','r',encoding = 'utf-8')
+data = file.read().split('##')
+file.close()
 
 for i in range(3):
-    del content[0]
+    del data[0]
 
-for c in content:
-    c1 = c.split['\n\n 1']
+for d in data:
+    d = d.split("\n\n")
+    p.append(d)
+print(p)
+# content = [{"title": x} for x in content ]
 
-content = [{"title": x} for x in content ]
 
-
-a = json.dumps(content, ensure_ascii=False)
+a = json.dumps(data, ensure_ascii=False)
 #print (a)
 
-file = open(filename, "w", encoding='utf-8')
+file = open('0.json', "w", encoding='utf-8')
 
-output = {0: content}
+output = {0: data}
 json.dump(output,file, ensure_ascii=False)
 file.close()
